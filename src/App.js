542a3897fc925a4d7869ec67d596140d111083ac
Nayan Kyada <nayan.kayada@gupshup.io>
2022-05-13 12:08:31 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,118 +1,52 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import "./style/index.css";
-import { useWeb3React } from "@web3-react/core";
-import { injected, walletconnect } from "./Connectore";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import createContract from "./contract";
-import { useNFTDrop } from "@thirdweb-dev/react";
+import {
+  useConnect,
+  useAccount,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+  useNetwork,
+  useBalance,
+} from "wagmi";
 
 function App() {
   const [errorMessage, setErrorMessage] = useState("");
-  const nftDrop = useNFTDrop("0xFb96438b418cB146133Bcb1B40E3A623b02a8D92");
-
   const {
-    connector,
-    chainId,
-    account,
-    activate,
-    deactivate,
-    active,
+    activeChain,
+    chains,
+    error: networkError,
+    isLoading,
+    pendingChainId,
+    switchNetwork,
+  } = useNetwork();
+  const {
+    activeConnector,
+    connect,
+    connectors,
     error,
-    library: web3,
-  } = useWeb3React();
-  const [balance, setBalance] = useState("");
+    isConnecting,
+    pendingConnector,
+  } = useConnect({
+   
+  });
 
-  const login = useCallback(
-    async (connector) => {
-      if (connector) {
-        await activate(connector, async (res, err) => {
-          // console.log(err, res);
-        });
-      }
-    },
-    [activate]
-  );
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    createContract(web3, "0x1F62AaBFa4aD4A5D782AF0cE0aBfF990ABf130E1")
-      .methods.safeMint(account)
-      .send({
-        from: account,
-        gas: "3000000",
-        value: web3.utils.toWei("0.01", "ether"),
-      })
-      .once("sending", function (payload) {
-        console.log("sending --- ", payload);
-      })
-      .once("sent", function (payload) {
-        console.log("sent --- ", payload);
-      })
-      .once("transactionHash", function (hash) {
-        console.log("transactionHash --- ", hash);
-      })
-      .once("receipt", function (receipt) {
-        console.log("receipt --- ", receipt);
-      })
-      .once("confirmation", function (confirmationNumber, receipt) {
-        console.log("confirmation --- ", confirmationNumber, receipt);
-      })
+  const { data: account } = useAccount();
+  const { data: balance } = useBalance({addressOrName:account?.address,watch:true});
 
-      .once("error", function (error) {
-        console.log("error --- ", error);
-      })
-      .then(function (receipt) {})
-      .catch((er) => console.log(er));
-  };
-
+  const { data: ensAvatar } = useEnsAvatar({ addressOrName: account?.address });
+  const { data: ensName } = useEnsName({ address: account?.address });
+  const { disconnect } = useDisconnect();
   useEffect(() => {
-    if (chainId) {
-      if (chainId !== 4) {
-        setErrorMessage("Connect your wallet with rinkeby network first");
-      } else {
-        setErrorMessage("");
-      }
+    if (account && activeChain?.id !== 4) {
+      setErrorMessage("Please Connect With Rinkeby Network");
     }
-  }, [chainId]);
-
-  useEffect(() => {
-    web3?.eth
-      .getBalance(account)
-      .then((p) => {
-        setBalance(web3?.utils.fromWei(p, "ether"));
-        console.log(p);
-      })
-      .catch((e) => console.log(e));
-  }, [account, chainId, active]);
-
-  const connectWithRinkeby = async () => {
-    if (connector instanceof WalletConnectConnector) {
-      await web3.currentProvider.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: web3.utils.toHex(4) }],
-      });
-    } else {
-      await window.ethereum.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: "0x4" }],
-      });
+    else{
+      setErrorMessage("")
     }
-  };
-  const logout = async () => {
-    if (connector instanceof WalletConnectConnector) {
-      await connector.close();
-      setErrorMessage("");
-      // window.location.reload();
-    } else {
-      deactivate();
-      setErrorMessage("");
-      // window.location.reload();
-    }
-  };
+  }, [activeChain]);
 
-  const mintPig = () => {
-    const quantity = 1; // how many unique NFTs you want to claim
-    nftDrop.claimTo(account, quantity).then((d) => console.log(d)).catch(e => console.log(e));
-  };
   return (
     <div className="App">
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -128,18 +62,15 @@ function App() {
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg ">
-          {errorMessage && (
+          {account && errorMessage && (
             <div className="rounded-md bg-red-50 mx-8 p-4  shadow-sm">
               <div className="flex justify-center">
                 <div className="ml-3">
                   <h3 className="text-sm font-medium text-red-600">
                     {errorMessage}
                   </h3>
-                  {account && (
-                    <button
-                      onClick={connectWithRinkeby}
-                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
+                  {"account" && (
+                    <button onClick={() => {switchNetwork(4)}} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                       Connect With Rinkeby
                     </button>
                   )}
@@ -153,18 +84,15 @@ function App() {
                 <dt
                   className="text-md font-extrabold cursor-pointer text-gray-500 truncate"
                   onClick={() => {
-                    navigator.clipboard.writeText(account);
+                    navigator.clipboard.writeText("account");
                   }}
                 >
-                  <abbr title="Click To Copy">{account}</abbr>
+                  <abbr title="Click To Copy">{account.address}</abbr>
                 </dt>
                 <dd className="mt-1 text-xl font-semibold text-gray-900">
-                  {Number(balance).toFixed(4)} ETH
+                  {balance && balance.formatted} ETH
                 </dd>
-                <button
-                  onClick={logout}
-                  className="mt-6 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
+                <button onClick={disconnect} className="mt-6 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   Disconnect Wallet
                 </button>
               </div>
@@ -173,14 +101,12 @@ function App() {
                   <div className="flex justify-between items-center">
                     <button
                       type="button"
-                      onClick={submitHandler}
                       className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Mint Bored Mutant ape
                     </button>
                     <button
                       type="button"
-                      onClick={mintPig}
                       className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Mint Pig
@@ -188,42 +114,27 @@ function App() {
                   </div>
                 </div>
               </div>
-              {/* <div
-                  className="mt-8"
-                  dangerouslySetInnerHTML={{
-                    __html: ` <iframe
-                  src="https://gateway.ipfscdn.io/ipfs/Qmb7uH1FozWt9zG15fYzmrBM9AvzvfQGU43hinmY4sEQTv/drop.html?contract=0xFb96438b418cB146133Bcb1B40E3A623b02a8D92&chainId=4"
-                  width="600px"
-                  height="600px"
-                  style="max-width:100%;"
-                ></iframe>`,
-                  }}
-                ></div> */}
             </div>
-          ) : !account ? (
+          ) : !account?.address ? (
             <div className="mx-8 flex space-x-3">
-              <button
-                onClick={() => {
-                  login(injected);
-                }}
-                type="submit"
-                className="mt-6 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Connect Metamask
-              </button>
-              <button
-                onClick={() => {
-                  login(walletconnect);
-                }}
-                type="submit"
-                className="mt-6 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Connect WalletConnect
-              </button>
+              {connectors.map((x) => (
+                <button
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  disabled={!x.ready}
+                  key={x.id}
+                  onClick={() => connect(x)}
+                >
+                  {x.name}
+                  {isConnecting &&
+                    pendingConnector?.id === x.id &&
+                    " (connecting)"}
+                </button>
+              ))}
             </div>
           ) : null}
         </div>
       </div>
+      {account && account.address}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,40 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Web3ReactProvider } from "@web3-react/core";
-import Web3 from "web3";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Provider, chain, createClient, defaultChains } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
-function getLibrary(provider) {
-  const library = new Web3(provider);
-  library.pollingInterval = 8000;
-  return library;
-}
+const infuraId = process.env.REACT_APP_INFURA_ID;
+
+const chains = defaultChains;
+const defaultChain = chain.mainnet;
+
+const client = createClient({
+  autoConnect: true,
+  connectors({ chainId }) {
+    const temps = chains.find((x) => x.id === chainId) ?? defaultChain;
+    const rpcUrl = temps.rpcUrls.infura
+      ? `${temps.rpcUrls.infura}/${infuraId}`
+      : temps.rpcUrls.default;
+    return [
+      new MetaMaskConnector({ chains: [chain.mainnet, chain.rinkeby] ,options: {
+        rpc: { [temps.id]: rpcUrl },
+      }}),
+      new WalletConnectConnector({
+        chains: [chain.mainnet, chain.rinkeby],
+        options: {
+          qrcode: true,
+          rpc: { [temps.id]: rpcUrl },
+        },
+      }),
+    ];
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <ThirdwebProvider desiredChainId={ChainId.Rinkeby} walletConnectors={[
-        "walletConnect",
-        { name: "injected", options: { shimDisconnect: false } },
-        {
-          name: "walletLink",
-          options: {
-            appName: "Example App",
-          },
-        },
-        {
-          name: "magic",
-          options: {
-            apiKey: "d5ffcd2812e4416d8d3fc5c43918286f",
-            rpcUrls: {
-              [ChainId.Rinkeby]: "https://rinkeby.infura.io/v3",
-            },
-          },
-        },
-      ]}>
-        <App />
-      </ThirdwebProvider>
-    </Web3ReactProvider>
+    <Provider client={client}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
